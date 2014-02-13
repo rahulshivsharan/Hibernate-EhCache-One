2ef5a36f68a617f1b9bbbf15defa97816c448d4c
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-02-13 21:35:49 +1200

    Create HibernateUtil.java

---
+++ src/com/hibernate/testing/HibernateUtil.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hibernate.testing;
+
+
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+
+/**
+ *
+ * @author rs69421
+ */
+public class HibernateUtil {
+
+    private static final SessionFactory SESSION_FACTORY = createSessionFactory();
+
+    private static SessionFactory createSessionFactory() {        
+        SessionFactory sessionFactory = null;        
+        try {            
+             
+            sessionFactory = new AnnotationConfiguration()
+                    .configure("conf/hibernate.cfg.xml").buildSessionFactory();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
+    }
+    
+    public static SessionFactory getSessionFactory(){
+        return SESSION_FACTORY;
+    }
+    
+    public static void shutdown(){
+        SESSION_FACTORY.close();
+    }
+}
